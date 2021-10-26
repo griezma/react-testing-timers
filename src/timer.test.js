e90d3ead0c11dd9630fa5f5c833f8087511a6bf3
griezma <griezma@outlook.com>
2021-10-26 15:16:44 +0200

    basic test added

---
+++ src/timer.test.js
@@ -0,0 +1,30 @@
+function timerGame(callback) {
+    setTimeout(() => {
+      callback && callback();
+    }, 10000);
+}
+
+beforeAll(() => {
+    jest.useFakeTimers();
+    jest.spyOn(global, 'setTimeout');
+})
+
+afterAll(() => {
+    jest.useRealTimers();
+    jest.clearAllMocks();
+});
+
+// seems to not be working as designed?!
+it.skip('calls the callback after 1 second via advanceTimersByTime', async () => {
+    const callback = jest.fn();
+  
+    timerGame(callback);
+
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    expect(callback).not.toBeCalled();
+  
+    jest.advanceTimersByTime(10000);
+
+    expect(callback).toBeCalled();
+    expect(callback).toHaveBeenCalledTimes(1);
+});
